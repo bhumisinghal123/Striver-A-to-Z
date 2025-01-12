d77edcb82ba0c0185bd24d63fdcde5bc0cf95557
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-01-12 18:50:08 +0530

    changes

--- Learn the basics/User_Input_Output.cpp
+++ Learn the basics/User_Input_Output.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main()
 {
-     int x, y;
+     int x, y, z;
+
      cin >> x >> y;
+
      cout << "The value of x: " << x << endl
           << "The value of y: " << y << endl;
+
+     return 0;
 }
