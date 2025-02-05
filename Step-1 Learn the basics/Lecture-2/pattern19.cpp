bf5ca7e8d88319a6d06e0117c7fb1603ade239cf
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-02-05 20:09:40 +0530

    patterns

---
+++ Step-1 Learn the basics/Lecture-2/pattern19.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int n)
+{
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n - i; j++)
+    {
+      cout << "*";
+    }
+    for (int j = 0; j < 2 * i; j++)
+    {
+      cout << " ";
+    }
+    for (int j = 0; j < n - i; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+  for (int i = 1; i <= n; i++)
+  {
+    for (int j = 0; j < i; j++)
+    {
+      cout << "*";
+    }
+    for (int j = 0; j < 2 * n - 2 * i; j++)
+    {
+      cout << " ";
+    }
+    for (int j = 0; j < i; j++)
+    {
+      cout << "*";
+    }
+    cout << endl;
+  }
+}
+
+int main()
+{
+  int t;
+  cin >> t;
+  for (int i = 0; i < t; i++)
+  {
+    int n;
+    cin >> n;
+    print(n);
+  }
+  return 0;
+}
