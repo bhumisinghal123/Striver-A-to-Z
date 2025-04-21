6138d3ba8c940459f66eb29607dfd1a38a34f98c
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-04-21 22:13:08 +0530

    array problems

--- Step-3 Solve problems on array/Lecture-3/4_Sum_Problem.cpp
+++ Step-3 Solve problems on array/Lecture-3/4_Sum_Problem.cpp
@@ -59,6 +59,7 @@ vector<vector<int>> fourSum(vector<int> &nums, int target)
 
   int n = nums.size();
   vector<vector<int>> ans;
+  sort(nums.begin(), nums.end());
   for (int i = 0; i < n; i++)
   {
     if (i > 0 && nums[i] == nums[i - 1])
