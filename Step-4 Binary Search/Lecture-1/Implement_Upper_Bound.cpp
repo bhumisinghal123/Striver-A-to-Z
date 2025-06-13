819c11cd05a1c9108c0eba9f7d45845ebfaa176b
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-06-13 12:07:28 +0530

    lower bound and uppar bound

---
+++ Step-4 Binary Search/Lecture-1/Implement_Upper_Bound.cpp
@@ -0,0 +1,56 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> arr, int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] >= x) {
+            // lower bound found:
+            return i;
+        }
+    }
+    return n;
+}
+
+int main()
+{
+    vector<int> arr = {3, 5, 8, 15, 19};
+    int n = 5, x = 9;
+    int ind = lowerBound(arr, n, x);
+    cout << "The lower bound is the index: " << ind << "\n";
+    return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> &nums, int n, int x)
+{
+  int low = 0, high = n - 1;
+  int ans = n;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+
+    if (nums[mid] > x)
+    {
+      ans = mid;
+      high = mid - 1;
+    }
+    else
+    {
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
+
+int main()
+{
+  vector<int> a = {1, 2, 3, 3, 5, 8, 8, 9, 9, 11};
+  int n = 10, x = 9;
+  int ind = lowerBound(a, n, x);
+  cout << "The lower bound is the index: " << ind << "\n";
+  return 0;
+}
