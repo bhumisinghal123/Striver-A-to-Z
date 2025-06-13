b104f14744b4593195bbe59a7b151d677c4604e8
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-06-13 15:39:29 +0530

    first and last occurence

---
+++ Step-4 Binary Search/Lecture-1/Find_the_first_or_last_occurrence.cpp
@@ -0,0 +1,134 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> &nums, int n, int x)
+{
+  int low = 0, high = n - 1;
+  int ans = n;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+
+    if (nums[mid] >= x)
+    {
+      ans = mid;
+      high = mid - 1;
+    }
+    else
+    {
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
+
+int upperBound(vector<int> &nums, int n, int x)
+{
+  int low = 0, high = n - 1;
+  int ans = n;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+
+    if (nums[mid] > x)
+    {
+      ans = mid;
+      high = mid - 1;
+    }
+    else
+    {
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
+
+pair<int, int> firstAndLastPostion(vector<int> &arr, int n, int k)
+{
+  int lb = lowerBound(arr, n, k);
+  if (lb == n || arr[lb] != k)
+    return {-1, -1};
+  return {lb, upperBound(arr, n, k) - 1};
+}
+int main()
+{
+  vector<int> a = {1, 2, 2, 6, 8, 8, 11, 13};
+  int n = 8, k = 8;
+  pair<int, int> ans = firstAndLastPostion(a, n, k);
+  cout << "The first and last position are: " << ans.first << " and " << ans.second << endl;
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int firstOccurence(vector<int> &arr, int n, int x)
+{
+  int low = 0, high = n - 1;
+  int first = -1;
+
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+    if (arr[mid] == x)
+    {
+      first = mid;
+      high = mid - 1;
+    }
+    else if (arr[mid] < x)
+    {
+      low = mid + 1;
+    }
+    else
+    {
+      high = mid - 1;
+    }
+  }
+  return first;
+}
+
+int lastOccurence(vector<int> &arr, int n, int x)
+{
+  int low = 0, high = n - 1;
+  int last = -1;
+
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+    if (arr[mid] == x)
+    {
+      last = mid;
+      low = mid + 1;
+    }
+    else if (arr[mid] < x)
+    {
+      low = mid + 1;
+    }
+    else
+    {
+      high = mid - 1;
+    }
+  }
+  return last;
+}
+
+pair<int, int> firstAndLastPostion(vector<int> &arr, int n, int x)
+{
+  int first = firstOccurence(arr, n, x);
+  if (first == -1)
+    return {-1, -1};
+  int last = lastOccurence(arr, n, x);
+  return {first, last};
+}
+
+int main()
+{
+  vector<int> a = {1, 2, 2, 6, 8, 8, 11, 13};
+  int n = 8, k = 8;
+  pair<int, int> ans = firstAndLastPostion(a, n, k);
+  cout << "The first and last position are: " << ans.first << " and " << ans.second << endl;
+  return 0;
+  return 0;
+}
--- Step-4 Binary Search/Lecture-1/Implement_Upper_Bound.cpp
+++ Step-4 Binary Search/Lecture-1/Implement_Upper_Bound.cpp
@@ -25,7 +25,7 @@ int main()
 #include <bits/stdc++.h>
 using namespace std;
 
-int lowerBound(vector<int> &nums, int n, int x)
+int upperBound(vector<int> &nums, int n, int x)
 {
   int low = 0, high = n - 1;
   int ans = n;
@@ -50,7 +50,7 @@ int main()
 {
   vector<int> a = {1, 2, 3, 3, 5, 8, 8, 9, 9, 11};
   int n = 10, x = 9;
-  int ind = lowerBound(a, n, x);
-  cout << "The lower bound is the index: " << ind << "\n";
+  int ind = upperBound(a, n, x);
+  cout << "The Upper bound is the index: " << ind << "\n";
   return 0;
 }
