7c49f99872c99ed62919316b48cbd87775b2bcfd
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-06-14 18:00:15 +0530

    number of time of rotation

--- Step-4 Binary Search/Lecture-1/Find_out_how_many_times_has_an_array_been_rotated.cpp
+++ Step-4 Binary Search/Lecture-1/Find_out_how_many_times_has_an_array_been_rotated.cpp
@@ -1,7 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int findMin(vector<int> &arr, int n)
+{
+  int low = 0, high = n - 1;
+  int ans = INT_MAX;
+  int index = -1;
+
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+    if (arr[low] <= arr[high])
+    {
+      if (arr[low] < ans)
+      {
+        index = low;
+        ans = arr[low];
+      }
+      break;
+    }
+    if (arr[low] <= arr[mid])
+    {
+      if (arr[low] < ans)
+      {
+        index = low;
+        ans = arr[low];
+      }
+      low = mid + 1;
+    }
+    else
+    {
+      high = mid - 1;
+      if (arr[mid] < ans)
+      {
+        index = mid;
+        ans = arr[mid];
+      }
+    }
+  }
+  return index;
+}
+
 int main()
 {
+  vector<int> a = {5, 6, 1, 2, 3, 4};
+  int n = 6;
+  int index = findMin(a, n);
+  cout << "The minimum in the array is: " << index << endl;
   return 0;
 }
