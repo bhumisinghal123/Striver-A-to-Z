cd944f2248e6f8c8af5f095b1aded7989a03643a
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-06-22 19:37:58 +0530

    Minimize Max Distance to Gas Station

---
+++ Step-4 Binary Search/Lecture-2/Minimize_Max_Distance_to_Gas_Station.cpp
@@ -0,0 +1,137 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+long double minimiseMaxDistance(vector<int> &arr, int k)
+{
+  int n = arr.size();
+  vector<int> howMany(n - 1, 0);
+  for (int gasStations = 1; gasStations <= k; gasStations++)
+  {
+    long double maxSection = -1;
+    int maxInd = -1;
+    for (int i = 0; i < n - 1; i++)
+    {
+      long double diff = (arr[i + 1] - arr[i]);
+      long double sectionLength = diff / (long double)(howMany[i] + 1);
+      if (sectionLength > maxSection)
+      {
+        maxSection = sectionLength;
+        maxInd = i;
+      }
+    }
+    howMany[maxInd]++;
+  }
+  long double maxAns = -1;
+  for (int i = 0; i < n - 1; i++)
+  {
+    long double diff = (arr[i + 1] - arr[i]);
+    long double sectionLength = diff / (long double)(howMany[i] + 1);
+    maxAns = max(maxAns, sectionLength);
+  }
+  return maxAns;
+}
+
+int main()
+{
+  vector<int> arr = {1, 2, 3, 4, 5};
+  int k = 4;
+  long double ans = minimiseMaxDistance(arr, k);
+  cout << "The answer is: " << ans << "\n";
+  return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+long double minimiseMaxDistance(vector<int> &arr, int k)
+{
+  int n = arr.size();
+  vector<int> howMany(n - 1, 0);
+  priority_queue<pair<long double, int>> pq;
+  for (int i = 0; i < n - 1; i++)
+  {
+    pq.push({arr[i + 1] - arr[i], i});
+  }
+  for (int gasStations = 1; gasStations <= k; gasStations++)
+  {
+    auto tp = pq.top();
+    pq.pop();
+    int secInd = tp.second;
+    howMany[secInd]++;
+    long double iniDiff = arr[secInd + 1] - arr[secInd];
+    long double newSecLen = iniDiff / (long double)(howMany[secInd] + 1);
+    pq.push({newSecLen, secInd});
+  }
+  return pq.top().first;
+}
+
+int main()
+{
+  vector<int> arr = {1, 2, 3, 4, 5};
+  int k = 4;
+  long double ans = minimiseMaxDistance(arr, k);
+  cout << "The answer is: " << ans << "\n";
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int numberOfGasStationsRequired(long double dist, vector<int> &arr)
+{
+  int n = arr.size(); // size of the array
+  int cnt = 0;
+  for (int i = 1; i < n; i++)
+  {
+    int numberInBetween = ((arr[i] - arr[i - 1]) / dist);
+    if ((arr[i] - arr[i - 1]) == (dist * numberInBetween))
+    {
+      numberInBetween--;
+    }
+    cnt += numberInBetween;
+  }
+  return cnt;
+}
+
+long double minimiseMaxDistance(vector<int> &arr, int k)
+{
+  int n = arr.size(); // size of the array
+  long double low = 0;
+  long double high = 0;
+
+  // Find the maximum distance:
+  for (int i = 0; i < n - 1; i++)
+  {
+    high = max(high, (long double)(arr[i + 1] - arr[i]));
+  }
+
+  // Apply Binary search:
+  long double diff = 1e-6;
+  while (high - low > diff)
+  {
+    long double mid = (low + high) / (2.0);
+    int cnt = numberOfGasStationsRequired(mid, arr);
+    if (cnt > k)
+    {
+      low = mid;
+    }
+    else
+    {
+      high = mid;
+    }
+  }
+  return high;
+}
+
+int main()
+{
+  vector<int> arr = {1, 2, 3, 4, 5};
+  int k = 4;
+  long double ans = minimiseMaxDistance(arr, k);
+  cout << "The answer is: " << ans << "\n";
+  return 0;
+}
