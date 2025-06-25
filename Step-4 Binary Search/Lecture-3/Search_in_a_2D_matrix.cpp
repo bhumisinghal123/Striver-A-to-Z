6c9b2710cf83edbc7de515a612d5f049829f43a2
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-06-25 21:35:09 +0530

    Search in a 2 D matrix

--- Step-4 Binary Search/Lecture-3/Search_in_a_2D_matrix.cpp
+++ Step-4 Binary Search/Lecture-3/Search_in_a_2D_matrix.cpp
@@ -25,3 +25,33 @@ int main()
   return 0;
 }
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchMatrix(vector<vector<int>> &mat, int target)
+{
+  int n = mat.size();
+  int m = mat[0].size();
+  int low = 0, high = n * m - 1;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+    int row = mid / m;
+    int col = mid % m;
+    if (mat[row][col] == target)
+      return true;
+    else if (mat[row][col] < target)
+      low = mid + 1;
+    else
+      high = mid - 1;
+  }
+  return false;
+}
+
+int main()
+{
+  vector<vector<int>> matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}};
+  searchMatrix(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+  return 0;
+}
