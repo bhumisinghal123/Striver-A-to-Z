4df6adbb8c116022f7e81b81891b3159fae2c457
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-06-25 21:17:02 +0530

    Find the row with maximum number of 1's

---
+++ Step-4 Binary Search/Lecture-3/Find_the_row_with_maximum_number_of_1s.cpp
@@ -0,0 +1,81 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int rowWithMax1s(vector<vector<int>> &matrix, int n, int m)
+{
+  int cnt_max = 0;
+  int index = -1;
+
+  // traverse the matrix:
+  for (int i = 0; i < n; i++)
+  {
+    int cnt_ones = 0;
+    for (int j = 0; j < m; j++)
+    {
+      cnt_ones += matrix[i][j];
+    }
+    if (cnt_ones > cnt_max)
+    {
+      cnt_max = cnt_ones;
+      index = i;
+    }
+  }
+  return index;
+}
+
+int main()
+{
+  vector<vector<int>> matrix = {{1, 1, 1}, {0, 0, 1}, {0, 0, 0}};
+  int n = 3, m = 3;
+  cout << "The row with maximum no. of 1's is: " << rowWithMax1s(matrix, n, m) << '\n';
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int lowerBound(vector<int> &nums, int n, int x)
+{
+  int low = 0, high = n - 1;
+  int ans = n;
+  while (low <= high)
+  {
+    int mid = (low + high) / 2;
+
+    if (nums[mid] >= x)
+    {
+      ans = mid;
+      high = mid - 1;
+    }
+    else
+    {
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
+
+int rowWithMax1s(vector<vector<int>> &matrix, int n, int m)
+{
+  int cnt_max = 0;
+  int index = -1;
+  for (int i = 0; i < n; i++)
+  {
+    int cnt_ones = m - lowerBound(matrix[i], m, 1);
+    if (cnt_ones > cnt_max)
+    {
+      cnt_max = cnt_ones;
+      index = i;
+    }
+  }
+  return index;
+}
+int main()
+{
+  vector<vector<int>> matrix = {{1, 1, 1}, {0, 0, 1}, {0, 0, 0}};
+  int n = 3, m = 3;
+  cout << "The row with maximum no. of 1's is: " << rowWithMax1s(matrix, n, m) << '\n';
+  return 0;
+}
