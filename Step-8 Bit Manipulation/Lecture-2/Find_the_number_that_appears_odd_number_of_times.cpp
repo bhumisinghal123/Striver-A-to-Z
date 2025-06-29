a10ed05f393c4e1c9b067a30f06f2059bfcb3e05
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-06-29 22:46:10 +0530

    ones and twos approach

--- Step-8 Bit Manipulation/Lecture-2/Find_the_number_that_appears_odd_number_of_times.cpp
+++ Step-8 Bit Manipulation/Lecture-2/Find_the_number_that_appears_odd_number_of_times.cpp
@@ -15,6 +15,7 @@ int main()
 }
 */
 
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -40,3 +41,54 @@ int main()
   cout << ans << endl;
   return 0;
 };
+*/
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNumber(vector<int> &nums)
+{
+  int n = nums.size();
+  sort(nums.begin(), nums.end());
+  for (int i = 1; i < n; i = i + 3)
+  {
+    if (nums[i] != nums[i - 1])
+    {
+      return nums[i - 1];
+    }
+  }
+  return nums[n - 1];
+}
+
+int main()
+{
+  vector<int> nums = {1, 3, 1, 2, 3, 3, 1};
+  int ans = singleNumber(nums);
+  cout << ans << endl;
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int singleNumber(vector<int> &nums)
+{
+  int ones = 0;
+  int twos = 0;
+  for (int i = 0; i < nums.size(); i++)
+  {
+    ones = (ones ^ nums[i]) & ~twos;
+    twos = (twos ^ nums[i]) & ~ones;
+  }
+  return ones;
+}
+
+int main()
+{
+  vector<int> nums = {1, 3, 1, 2, 3, 3, 1};
+  int ans = singleNumber(nums);
+  cout << ans << endl;
+  return 0;
+}
