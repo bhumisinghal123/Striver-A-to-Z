a698a4cb597cd16d71e6ea6c6ce4d24565871a02
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-06-29 22:16:18 +0530

    number appers 3

--- Step-8 Bit Manipulation/Lecture-2/Find_the_number_that_appears_odd_number_of_times.cpp
+++ Step-8 Bit Manipulation/Lecture-2/Find_the_number_that_appears_odd_number_of_times.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,3 +13,30 @@ int main()
   cout << "Resulting XOR: " << xorr << endl;
   return 0;
 }
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  vector<int> nums = {1, 3, 1, 2, 3, 3, 1};
+  int ans = 0;
+  for (int bitIndex = 0; bitIndex <= 31; bitIndex++)
+  {
+    int cnt = 0;
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if ((nums[i] & (1 << bitIndex)))
+      {
+        cnt++;
+      }
+    }
+    if (cnt % 3 == 1)
+    {
+      ans = (ans | (1 << bitIndex));
+    }
+  }
+  cout << ans << endl;
+  return 0;
+};
