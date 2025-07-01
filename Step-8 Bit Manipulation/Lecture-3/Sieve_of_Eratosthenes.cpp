3fc0e27097d27e1ac3e9150c436e9027dccebaf9
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-07-01 21:17:50 +0530

    Sieve of Eratosthenes

---
+++ Step-8 Bit Manipulation/Lecture-3/Sieve_of_Eratosthenes.cpp
@@ -0,0 +1,80 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+bool prime(int n)
+{
+  int cnt = 0;
+  for (int i = 1; i * i <= n; i++)
+  {
+    if (n % i == 0)
+    {
+      cnt++;
+      if ((n / i) != i)
+      {
+        cnt++;
+      }
+    }
+  }
+  if (cnt == 2)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+int primeNo(int n)
+{
+  for (int i = 2; i <= n; i++)
+  {
+    if (prime(i))
+      cout<<i<<" ";
+  }
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  primeNo(n);
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int primeNo(int n)
+{
+  int prime[n + 1];
+  for (int i = 2; i <= n; i++)
+  {
+    prime[i] = 1;
+  }
+  for (int i = 2; i * i <= n; i++)
+  {
+    if (prime[i] == 1)
+    {
+      for (int j = i * i; j <= n; j += i)
+      {
+        prime[j] = 0;
+      }
+    }
+  }
+  for (int i = 2; i <= n; i++)
+  {
+    if (prime[i] == 1)
+      cout << i << " ";
+  }
+}
+
+int main()
+{
+  int n;
+  cin >> n;
+  primeNo(n);
+  return 0;
+}
