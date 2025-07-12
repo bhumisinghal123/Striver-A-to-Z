125b0bf0b71e0df3c24187a397d1655beaf42d09
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-07-12 22:25:09 +0530

    Introduction to linked list

---
+++ Step-6 LinkedList/Lecture-1/Introduction_to_LinkedList.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// struct || class
+struct Node
+{
+public:
+  int data;
+  Node *next;
+
+public:
+  Node(int data1, Node *next1)
+  {
+    data = data1;
+    next = next1;
+  }
+
+public:
+  Node(int data1)
+  {
+    data = data1;
+    next = nullptr;
+  }
+};
+
+Node *convertArr2LL(vector<int> &arr)
+{
+  Node *head = new Node(arr[0]);
+  Node *mover = head;
+  for (int i = 1; i < arr.size(); i++)
+  {
+    Node *temp = new Node(arr[i]);
+    mover->next = temp;
+    mover = temp;
+  }
+  return head;
+}
+
+int lengthOfLL(Node *head)
+{
+  int cnt = 0;
+  Node *temp = head;
+  while (temp)
+  {
+    temp = temp->next;
+    cnt++;
+  }
+  return cnt;
+}
+
+int checkIfPresent(Node *head, int val)
+{
+  Node *temp = head;
+  while (temp)
+  {
+    if (temp->data == val)
+      return 1;
+    temp = temp->next;
+  }
+  return 0;
+}
+
+int main()
+{
+  vector<int> arr = {2, 5, 8, 7};
+  // Node *y = new Node{arr[0]};
+  // cout << y->data;
+
+  Node *head = convertArr2LL(arr);
+  // Node *temp = head;
+  // while (temp)
+  // {
+  //   cout << temp->data << " ";
+  //   temp = temp->next;
+  // }
+
+  // cout << lengthOfLL(head);
+
+  cout << checkIfPresent(head, 15);
+  return 0;
+}
