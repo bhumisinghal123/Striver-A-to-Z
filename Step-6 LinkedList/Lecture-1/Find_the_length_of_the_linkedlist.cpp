908c4fb410873841fc2a5087a03506adde659dca
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-07-13 22:01:09 +0530

    Find the length of the linkedlist [learn traversal]

--- Step-6 LinkedList/Lecture-1/Find_the_length_of_the_linkedlist.cpp
+++ Step-6 LinkedList/Lecture-1/Find_the_length_of_the_linkedlist.cpp
@@ -36,32 +36,22 @@ Node *convertArr2LL(vector<int> &arr)
   return head;
 }
 
-int checkIfPresent(Node *head, int val)
+int lengthOfLL(Node *head)
 {
+  int cnt = 0;
   Node *temp = head;
   while (temp)
   {
-    if (temp->data == val)
-      return 1;
     temp = temp->next;
+    cnt++;
   }
-  return 0;
+  return cnt;
 }
 
 int main()
 {
   vector<int> arr = {2, 5, 8, 7};
-  // Node *y = new Node{arr[0]};
-  // cout << y->data;
-
   Node *head = convertArr2LL(arr);
-  // Node *temp = head;
-  // while (temp)
-  // {
-  //   cout << temp->data << " ";
-  //   temp = temp->next;
-  // }
-
-   cout << checkIfPresent(head, 15);
+  cout << lengthOfLL(head);
   return 0;
 }
