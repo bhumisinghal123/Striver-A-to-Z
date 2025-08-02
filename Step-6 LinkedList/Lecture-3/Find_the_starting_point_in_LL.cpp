cd7bb12d393435f5e12875f018dad9a4fdfdb3a7
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-08-02 22:09:38 +0530

    Find the starting point in LL

---
+++ Step-6 LinkedList/Lecture-3/Find_the_starting_point_in_LL.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+  int data;
+  Node *next;
+  Node(int data1, Node *next1)
+  {
+    data = data1;
+    next = next1;
+  }
+  Node(int data1)
+  {
+    data = data1;
+    next = nullptr;
+  }
+};
+
+/*
+Node *detectLoop(Node *head)
+{
+  Node *temp = head;
+  unordered_map<Node *, int> mp;
+  while (temp != NULL)
+  {
+    if (mp.count(temp) != 0)
+    {
+      return temp;
+    }
+    mp[temp] = 1;
+    temp = temp->next;
+  }
+  return nullptr;
+}
+*/
+
+Node *detectLoop(Node *head)
+{
+  Node *slow = head;
+  Node *fast = head;
+  while (fast != NULL && fast->next != NULL)
+  {
+    slow = slow->next;
+    fast = fast->next->next;
+    if (slow == fast)
+    {
+      slow = head;
+      while (slow != fast)
+      {
+        slow = slow->next;
+        fast = fast->next;
+      }
+      return slow;
+    }
+  }
+  return NULL;
+}
+
+int main()
+{
+  Node *node1 = new Node(1);
+  Node *node2 = new Node(2);
+  Node *node3 = new Node(3);
+  Node *node4 = new Node(4);
+  Node *node5 = new Node(5);
+  node1->next = node2;
+  node2->next = node3;
+  node3->next = node4;
+  node4->next = node5;
+  node5->next = node2;
+  Node *head = node1;
+  Node *loopStartNode = detectLoop(head);
+  if (loopStartNode)
+  {
+    cout << "Loop detected. Starting node of the loop is: " << loopStartNode->data << endl;
+  }
+  else
+  {
+    cout << "No loop detected in the linked list." << endl;
+  }
+  return 0;
+}
