9e9734cb2e5f9ed5e59100ba7c74a30d69302a1d
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-09-30 00:08:29 +0530

    Print all permutation of array or string

---
+++ Step-7 Recursion/Lecture-extra/Print_all_permutation_of_string_and_array.cpp
@@ -0,0 +1,98 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+  void recurPermute(vector<int> &ds, vector<int> &nums, vector<vector<int>> &ans, int freq[])
+  {
+    if (ds.size() == nums.size())
+    {
+      ans.push_back(ds);
+      return;
+    }
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if (!freq[i])
+      {
+        ds.push_back(nums[i]);
+        freq[i] = 1;
+        recurPermute(ds, nums, ans, freq);
+        freq[i] = 0;
+        ds.pop_back();
+      }
+    }
+  }
+
+public:
+  vector<vector<int>> permute(vector<int> &nums)
+  {
+    vector<vector<int>> ans;
+    vector<int> ds;
+    int freq[nums.size()] = {0};
+    for (int i = 0; i < nums.size(); i++)
+      freq[i] = 0;
+    recurPermute(ds, nums, ans, freq);
+    return ans;
+  }
+};
+
+int main()
+{
+  vector<int> arr = {1, 2, 3};
+  Solution obj;
+  vector<vector<int>> ans = obj.permute(arr);
+  for (auto i : ans)
+  {
+    for (auto j : i)
+      cout << j << " ";
+    cout << endl;
+  }
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+  void recurPermute(int index, vector<int> &nums, vector<vector<int>> &ans)
+  {
+    if (index == nums.size())
+    {
+      ans.push_back(nums);
+      return;
+    }
+    for (int i = index; i < nums.size(); i++)
+    {
+      swap(nums[index], nums[i]);
+      recurPermute(index + 1, nums, ans);
+      swap(nums[index], nums[i]);
+    }
+  }
+
+public:
+  vector<vector<int>> permute(vector<int> &nums)
+  {
+    vector<vector<int>> ans;
+    recurPermute(0, nums, ans);
+    return ans;
+  }
+};
+
+int main()
+{
+  vector<int> arr = {1, 2, 3};
+  Solution obj;
+  vector<vector<int>> ans = obj.permute(arr);
+  for (auto i : ans)
+  {
+    for (auto j : i)
+      cout << j << " ";
+    cout << endl;
+  }
+  return 0;
+}
