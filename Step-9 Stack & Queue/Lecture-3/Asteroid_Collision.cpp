8a15456ac234f850078ea6f810de0896307a7489
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-09-30 19:46:11 +0530

    asteroid problem

--- Step-9 Stack & Queue/Lecture-3/Asteroid_Collision.cpp
+++ Step-9 Stack & Queue/Lecture-3/Asteroid_Collision.cpp
@@ -1,7 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution
+{
+public:
+  vector<int> asteroidCollision(vector<int> &asteroids)
+  {
+    int n = asteroids.size();
+    vector<int> st;
+    for (int i = 0; i < n; i++)
+    {
+      if (asteroids[i] > 0)
+      {
+        st.push_back(asteroids[i]);
+      }
+      else
+      {
+        while (!st.empty() && st.back() > 0 && st.back() < abs(asteroids[i]))
+        {
+          st.pop_back();
+        }
+        if (!st.empty() && st.back() == abs(asteroids[i]))
+        {
+          st.pop_back();
+        }
+        else if (st.empty() || st.back() < 0)
+        {
+          st.push_back(asteroids[i]);
+        }
+      }
+    }
+    return st;
+  }
+};
+
 int main()
 {
+  vector<int> asteroids = {5, 10, -5};
+  Solution sol;
+  vector<int> result = sol.asteroidCollision(asteroids);
+  for (int asteroid : result)
+  {
+    cout << asteroid << " ";
+  }
   return 0;
 }
