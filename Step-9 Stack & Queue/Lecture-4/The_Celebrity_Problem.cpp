17ef5e00ce5c303faebc321f8b52edb12702770a
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-10-02 19:26:48 +0530

    The Celebrity Problem

---
+++ Step-9 Stack & Queue/Lecture-4/The_Celebrity_Problem.cpp
@@ -0,0 +1,107 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int celebrity(vector<vector<int>> &M)
+  {
+    int n = M.size();
+    vector<int> knowMe(n, 0);
+    vector<int> Iknow(n, 0);
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < n; j++)
+      {
+        if (M[i][j] == 1)
+        {
+          knowMe[j]++;
+          Iknow[i]++;
+        }
+      }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+      if (knowMe[i] == n - 1 && Iknow[i] == 0)
+      {
+        return i;
+      }
+    }
+    return -1;
+  }
+};
+
+int main()
+{
+  vector<vector<int>> M = {
+      {0, 1, 1, 0},
+      {0, 0, 0, 0},
+      {1, 1, 0, 0},
+      {0, 1, 1, 0}};
+
+  Solution sol;
+  int ans = sol.celebrity(M);
+  cout << "The index of celebrity is: " << ans;
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int celebrity(vector<vector<int>> &M)
+  {
+    int n = M.size();
+    int top = 0, down = n - 1;
+    while (top < down)
+    {
+      if (M[top][down] == 1)
+      {
+        top = top + 1;
+      }
+      else if (M[down][top] == 1)
+      {
+        down = down - 1;
+      }
+      else
+      {
+        top++;
+        down--;
+      }
+    }
+
+    if (top > down)
+      return -1;
+
+    for (int i = 0; i < n; i++)
+    {
+      if (i == top)
+        continue;
+
+      if (M[top][i] == 1 || M[i][top] == 0)
+      {
+        return -1;
+      }
+    }
+    return top;
+  }
+};
+
+int main()
+{
+  vector<vector<int>> M = {
+      {0, 1, 1, 0},
+      {0, 0, 0, 0},
+      {1, 1, 0, 0},
+      {0, 1, 1, 0}};
+
+  Solution sol;
+  int ans = sol.celebrity(M);
+  cout << "The index of celebrity is: " << ans;
+  return 0;
+}
