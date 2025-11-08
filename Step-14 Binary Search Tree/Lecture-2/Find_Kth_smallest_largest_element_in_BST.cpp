e22efd32b717976d7efb41d9a9698d197c7a017d
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-11-08 16:15:22 +0530

    Find K-th smallest/largest element in BST

---
+++ Step-14 Binary Search Tree/Lecture-2/Find_Kth_smallest_largest_element_in_BST.cpp
@@ -0,0 +1,169 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+private:
+  void inorder(TreeNode *node, vector<int> &arr)
+  {
+    if (!node)
+    {
+      return;
+    }
+    inorder(node->left, arr);
+    arr.push_back(node->val);
+    inorder(node->right, arr);
+    return;
+  }
+
+public:
+  pair<int, int> findKth(TreeNode *node, int k)
+  {
+    vector<int> arr;
+    inorder(node, arr);
+    int kLargest = arr[arr.size() - k];
+    int kSmallest = arr[k - 1];
+    return make_pair(kSmallest, kLargest);
+  }
+};
+
+void printInOrder(TreeNode *root)
+{
+  if (root == nullptr)
+  {
+    return;
+  }
+  printInOrder(root->left);
+  cout << root->val << " ";
+  printInOrder(root->right);
+}
+
+int main()
+{
+  TreeNode *root = new TreeNode(10);
+  root->left = new TreeNode(5);
+  root->right = new TreeNode(13);
+  root->left->left = new TreeNode(3);
+  root->left->left->left = new TreeNode(2);
+  root->left->left->right = new TreeNode(4);
+  root->left->right = new TreeNode(6);
+  root->left->right->right = new TreeNode(9);
+  root->right->left = new TreeNode(11);
+  root->right->right = new TreeNode(14);
+
+  cout << "Binary Search Tree: " << endl;
+  printInOrder(root);
+  cout << endl;
+
+  Solution solution;
+  int k = 3;
+  cout << "k: " << k << endl;
+  pair<int, int> kthElements = solution.findKth(root, k);
+
+  cout << "Kth smallest element: " << kthElements.first << endl;
+  cout << "Kth largest element: " << kthElements.second << endl;
+
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+private:
+  void reverseInorder(TreeNode *node, int &counter, int k, int &kLargest)
+  {
+    if (!node || counter >= k)
+      return;
+    reverseInorder(node->right, counter, k, kLargest);
+    counter++;
+    if (counter == k)
+    {
+      kLargest = node->val;
+      return;
+    }
+    reverseInorder(node->left, counter, k, kLargest);
+  }
+
+  void inorder(TreeNode *node, int &counter, int k, int &kSmallest)
+  {
+    if (!node || counter >= k)
+      return;
+    inorder(node->left, counter, k, kSmallest);
+    counter++;
+    if (counter == k)
+    {
+      kSmallest = node->val;
+      return;
+    }
+    inorder(node->right, counter, k, kSmallest);
+  }
+
+public:
+  pair<int, int> findKth(TreeNode *root, int k)
+  {
+    int kSmallest = INT_MIN, kLargest = INT_MIN;
+    int counter = 0;
+    inorder(root, counter, k, kSmallest);
+    counter = 0;
+    reverseInorder(root, counter, k, kLargest);
+    return make_pair(kSmallest, kLargest);
+  }
+};
+
+void printInOrder(TreeNode *root)
+{
+  if (root == nullptr)
+  {
+    return;
+  }
+  printInOrder(root->left);
+  cout << root->val << " ";
+  printInOrder(root->right);
+}
+
+int main()
+{
+  TreeNode *root = new TreeNode(10);
+  root->left = new TreeNode(5);
+  root->right = new TreeNode(13);
+  root->left->left = new TreeNode(3);
+  root->left->left->left = new TreeNode(2);
+  root->left->left->right = new TreeNode(4);
+  root->left->right = new TreeNode(6);
+  root->left->right->right = new TreeNode(9);
+  root->right->left = new TreeNode(11);
+  root->right->right = new TreeNode(14);
+
+  cout << "Binary Search Tree: " << endl;
+  printInOrder(root);
+  cout << endl;
+  Solution solution;
+  int k = 3;
+  cout << "k: " << k << endl;
+  pair<int, int> kthElements = solution.findKth(root, k);
+
+  cout << "Kth smallest element: " << kthElements.first << endl;
+  cout << "Kth largest element: " << kthElements.second << endl;
+
+  return 0;
+}
