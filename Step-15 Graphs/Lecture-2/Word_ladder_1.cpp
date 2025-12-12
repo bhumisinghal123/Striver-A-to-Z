9b7a7a45d77dc8b0bd4e26b4914d414b796147d2
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-12-12 19:42:58 +0530

    Word ladder - 1

---
+++ Step-15 Graphs/Lecture-2/Word_ladder_1.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int wordLadderLength(string startWord, string targetWord, vector<string> &wordList)
+  {
+    queue<pair<string, int>> q;
+    q.push({startWord, 1});
+    unordered_set<string> st(wordList.begin(), wordList.end());
+    st.erase(startWord);
+    while (!q.empty())
+    {
+      string word = q.front().first;
+      int steps = q.front().second;
+      q.pop();
+
+      if (word == targetWord)
+        return steps;
+
+      for (int i = 0; i < word.size(); i++)
+      {
+        char original = word[i];
+        for (char ch = 'a'; ch <= 'z'; ch++)
+        {
+          word[i] = ch;
+          if (st.find(word) != st.end())
+          {
+            st.erase(word);
+            q.push({word, steps + 1});
+          }
+        }
+        word[i] = original;
+      }
+    }
+    return 0;
+  }
+};
+
+int main()
+{
+  Solution obj;
+  string startWord = "hit";
+  string targetWord = "cog";
+  vector<string> wordList = {"hot", "dot", "dog", "lot", "log", "cog"};
+  int ans = obj.wordLadderLength(startWord, targetWord, wordList);
+  cout << ans << endl;
+  return 0;
+}
