c18a6275c1598cc1f6e133ce8aba5f5ad614f085
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-12-21 22:09:01 +0530

    Minimum steps to reach end from start by performing multiplication and mod operations with array elements

---
+++ Step-15 Graphs/Lecture-4/Minimum_steps_to_reach_end_from_start.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int minimumMultiplications(vector<int> &arr, int start, int end)
+  {
+    queue<pair<int, int>> q;
+    q.push({start, 0});
+    vector<int> dist(100000, 1e9);
+    dist[start] = 0;
+    int mod = 100000;
+    while (!q.empty())
+    {
+      int node = q.front().first;
+      int steps = q.front().second;
+      q.pop();
+
+      for (auto it : arr)
+      {
+        int num = (it * node) % mod;
+        if (steps + 1 < dist[num])
+        {
+          dist[num] = steps + 1;
+          if (num == end)
+            return steps + 1;
+          q.push({num, steps + 1});
+        }
+      }
+    }
+    return -1;
+  }
+};
+
+int main()
+{
+  Solution sol;
+  vector<int> arr = {2, 5, 7};
+  int start = 3;
+  int end = 30;
+  int result = sol.minimumMultiplications(arr, start, end);
+  cout << "Minimum multiplications to reach " << end << " from " << start << " is: " << result << endl;
+  return 0;
+}
