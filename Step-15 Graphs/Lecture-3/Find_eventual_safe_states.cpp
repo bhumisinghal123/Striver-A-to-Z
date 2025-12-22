18d43a2fd92ea3f9f80bd2a4fb12bc34ca2501a0
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-12-22 22:52:54 +0530

    safe state using dfs

--- Step-15 Graphs/Lecture-3/Find_eventual_safe_states.cpp
+++ Step-15 Graphs/Lecture-3/Find_eventual_safe_states.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -42,6 +43,90 @@ public:
     }
 
     sort(safeNodes.begin(), safeNodes.end());
+    return safeNodes;
+  }
+};
+
+int main()
+{
+  int V = 7;
+  vector<int> adj[V];
+  adj[0] = {1, 2};
+  adj[1] = {2, 3};
+  adj[2] = {5};
+  adj[3] = {0};
+  adj[4] = {5};
+  adj[5] = {};
+  adj[6] = {4, 5};
+
+  Solution sol;
+  vector<int> safeNodes = sol.eventualSafeNodes(V, adj);
+
+  cout << "Eventual Safe Nodes: ";
+  for (int node : safeNodes)
+  {
+    cout << node << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+
+private:
+  bool dfsCheck(int node, vector<int> adj[], int vis[], int pathVis[], int check[])
+  {
+    vis[node] = 1;
+    pathVis[node] = 1;
+    check[node] = 0;
+
+    for (auto it : adj[node])
+    {
+      if (!vis[it])
+      {
+        if (dfsCheck(it, adj, vis, pathVis, check) == true)
+        {
+          check[node] = 0;
+          return true;
+        }
+      }
+      else if (pathVis[it])
+      {
+        check[node] = 0;
+        return true;
+      }
+    }
+    check[node] = 1;
+    pathVis[node] = 0;
+    return false;
+  }
+
+public:
+  vector<int> eventualSafeNodes(int V, vector<int> adj[])
+  {
+    int vis[V] = {0};
+    int pathVis[V] = {0};
+    int check[V] = {0};
+    vector<int> safeNodes;
+
+    for (int i = 0; i < V; i++)
+    {
+      if (!vis[i])
+      {
+        dfsCheck(i, adj, vis, pathVis, check);
+      }
+    }
+    for (int i = 0; i < V; i++)
+    {
+      if (check[i] == 1)
+        safeNodes.push_back(i);
+    }
     return safeNodes;
   }
 };
