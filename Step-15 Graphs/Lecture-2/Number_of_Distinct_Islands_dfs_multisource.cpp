8aa5d0ac97013ff6f854dc7c9bad0a7566c3acef
Bhumi Singhal <bhumisinghal02@gmail.com>
2025-12-22 22:14:52 +0530

    Number of distinct island

--- Step-15 Graphs/Lecture-2/Number_of_Distinct_Islands_dfs_multisource.cpp
+++ Step-15 Graphs/Lecture-2/Number_of_Distinct_Islands_dfs_multisource.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -66,6 +67,72 @@ int main()
 
   Solution obj;
   int result = obj.numIslands(grid);
+  cout << "Number of distinct islands: " << result << endl;
+
+  return 0;
+}
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+  void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>> &grid, vector<pair<int, int>> &vec, int row0, int col0)
+  {
+    int n = grid.size();
+    int m = grid[0].size();
+    vis[row][col] = 1;
+    vec.push_back({row - row0, col - col0});
+
+    int delrow[] = {-1, 0, 1, 0};
+    int delcol[] = {0, 1, 0, -1};
+    for (int i = 0; i < 4; i++)
+    {
+      int nrow = row + delrow[i];
+      int ncol = col + delcol[i];
+      if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && !vis[nrow][ncol])
+      {
+        dfs(nrow, ncol, vis, grid, vec, row0, col0);
+      }
+    }
+  }
+
+public:
+  int countDistinctIslands(vector<vector<int>> &grid)
+  {
+    int n = grid.size();
+    int m = grid[0].size();
+    vector<vector<int>> vis(n, vector<int>(m, 0));
+    set<vector<pair<int, int>>> st;
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 0; j < m; j++)
+      {
+        if (!vis[i][j] && grid[i][j] == 1)
+        {
+          vector<pair<int, int>> vec;
+          dfs(i, j, vis, grid, vec, i, j);
+          st.insert(vec);
+        }
+      }
+    }
+    return st.size();
+  }
+};
+
+int main()
+{
+  vector<vector<int>> grid = {
+      {1, 1, 0, 0, 0},
+      {1, 0, 0, 1, 1},
+      {0, 0, 0, 1, 0},
+      {0, 1, 0, 0, 0}};
+
+  Solution obj;
+  int result = obj.countDistinctIslands(grid);
   cout << "Number of distinct islands: " << result << endl;
 
   return 0;
