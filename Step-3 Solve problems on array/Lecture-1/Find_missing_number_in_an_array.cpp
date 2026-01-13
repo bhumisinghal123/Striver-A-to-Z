e78c0541f61a7e1450eef323dc6d0e8a05742ec8
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-01-14 00:35:08 +0530

    change and MST

--- Step-3 Solve problems on array/Lecture-1/Find_missing_number_in_an_array.cpp
+++ Step-3 Solve problems on array/Lecture-1/Find_missing_number_in_an_array.cpp
@@ -1,3 +1,80 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int missingNumber(vector<int> &a, int N)
+  {
+    for (int i = 1; i <= N; i++)
+    {
+      int flag = 0;
+
+      for (int j = 0; j < N - 1; j++)
+      {
+        if (a[j] == i)
+        {
+          flag = 1;
+          break;
+        }
+      }
+
+      if (flag == 0)
+        return i;
+    }
+
+    return -1;
+  }
+};
+
+int main()
+{
+  int N = 5;
+  vector<int> a = {1, 2, 4, 5};
+
+  Solution obj;
+  int ans = obj.missingNumber(a, N);
+  cout << "The missing number is: " << ans << endl;
+
+  return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int missingNumber(vector<int> &a, int N)
+  {
+    int sum = (N * (N + 1)) / 2;
+
+    int s2 = 0;
+    for (int i = 0; i < N - 1; i++)
+    {
+      s2 += a[i];
+    }
+
+    return sum - s2;
+  }
+};
+
+int main()
+{
+  int N = 5;
+  vector<int> a = {1, 2, 4, 5};
+
+  Solution obj;
+  int ans = obj.missingNumber(a, N);
+  cout << "The missing number is: " << ans << endl;
+
+  return 0;
+}
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
