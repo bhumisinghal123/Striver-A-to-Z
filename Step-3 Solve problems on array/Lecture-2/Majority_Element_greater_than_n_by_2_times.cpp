2f7cb0be842cd02fd4dbeffb3e23663ce27c3c25
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-01-16 22:49:59 +0530

    changes and disjoint set union

--- Step-3 Solve problems on array/Lecture-2/Majority_Element_greater_than_n_by_2_times.cpp
+++ Step-3 Solve problems on array/Lecture-2/Majority_Element_greater_than_n_by_2_times.cpp
@@ -1,9 +1,59 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int majorityElement(vector<int> &nums)
+  {
+
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+
+      int cnt = 0;
+
+      for (int j = 0; j < n; j++)
+      {
+        if (nums[j] == nums[i])
+        {
+          cnt++;
+        }
+      }
+
+      if (cnt > (n / 2))
+      {
+        return nums[i];
+      }
+    }
+
+    return -1;
+  }
+};
+
+int main()
+{
+  vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+
+  Solution sol;
+
+  int ans = sol.majorityElement(arr);
+
+  cout << "The majority element is: " << ans << endl;
+
+  return 0;
+}
+*/
+
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
 int majorityElement(vector<int> v)
 {
-  /*
+
   map<int, int> mpp;
   for (int i = 0; i < v.size(); i++)
   {
@@ -15,37 +65,6 @@ int majorityElement(vector<int> v)
     {
       return it.first;
     }
-  }
-  return -1;
-  */
-
-  int cnt = 0;
-  int el;
-  for (int i = 0; i < v.size(); i++)
-  {
-    if (cnt == 0)
-    {
-      cnt = 1;
-      el = v[i];
-    }
-    else if (v[i] == el)
-    {
-      cnt++;
-    }
-    else
-    {
-      cnt--;
-    }
-  }
-  int cnt1 = 0;
-  for (int i = 0; i < v.size(); i++)
-  {
-    if (v[i] == el)
-      cnt1++;
-  }
-  if (cnt1 > (v.size() / 2))
-  {
-    return el;
   }
   return -1;
 }
@@ -60,5 +79,69 @@ int main()
     cin >> arr[i];
   }
   cout << majorityElement(arr);
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int majorityElement(vector<int> &nums)
+  {
+
+    int n = nums.size();
+
+    int cnt = 0;
+
+    int el;
+
+    for (int i = 0; i < n; i++)
+    {
+      if (cnt == 0)
+      {
+        cnt = 1;
+        el = nums[i];
+      }
+      else if (el == nums[i])
+      {
+        cnt++;
+      }
+      else
+      {
+        cnt--;
+      }
+    }
+
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++)
+    {
+      if (nums[i] == el)
+      {
+        cnt1++;
+      }
+    }
+
+    if (cnt1 > (n / 2))
+    {
+      return el;
+    }
+
+    return -1;
+  }
+};
+
+int main()
+{
+  vector<int> arr = {2, 2, 1, 1, 1, 2, 2};
+
+  Solution sol;
+
+  int ans = sol.majorityElement(arr);
+
+  cout << "The majority element is: " << ans << endl;
+
   return 0;
 }
--- Step-3 Solve problems on array/Lecture-2/Sort_an_array_of_0s_1s_and_2s.cpp
+++ Step-3 Solve problems on array/Lecture-2/Sort_an_array_of_0s_1s_and_2s.cpp
@@ -1,3 +1,115 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  void sortZeroOneTwo(vector<int> &nums)
+  {
+    int count0 = 0, count1 = 0, count2 = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if (nums[i] == 0)
+        count0++;
+      else if (nums[i] == 1)
+        count1++;
+      else
+        count2++;
+    }
+
+    int index = 0;
+
+    while (count0--)
+    {
+      nums[index++] = 0;
+    }
+
+    while (count1--)
+    {
+      nums[index++] = 1;
+    }
+
+    while (count2--)
+    {
+      nums[index++] = 2;
+    }
+  }
+};
+
+int main()
+{
+  vector<int> nums = {1, 0, 2, 1, 0};
+
+  Solution obj;
+  obj.sortZeroOneTwo(nums);
+
+  for (int x : nums)
+  {
+    cout << x << " ";
+  }
+
+  return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  void sortZeroOneTwo(vector<int> &nums)
+  {
+    int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      if (nums[i] == 0)
+        cnt0++;
+      else if (nums[i] == 1)
+        cnt1++;
+      else
+        cnt2++;
+    }
+
+    for (int i = 0; i < cnt0; i++)
+    {
+      nums[i] = 0;
+    }
+
+    for (int i = cnt0; i < cnt0 + cnt1; i++)
+    {
+      nums[i] = 1;
+    }
+
+    for (int i = cnt0 + cnt1; i < nums.size(); i++)
+    {
+      nums[i] = 2;
+    }
+  }
+};
+
+int main()
+{
+  vector<int> nums = {0, 2, 1, 2, 0, 1};
+
+  Solution sol;
+  sol.sortZeroOneTwo(nums);
+
+  cout << "After sorting:" << endl;
+  for (int i = 0; i < nums.size(); i++)
+  {
+    cout << nums[i] << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
