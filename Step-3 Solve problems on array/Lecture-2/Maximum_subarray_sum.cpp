8e7a8bd0e4391238722c3b123f0dc8e73393bc44
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-01-16 23:26:38 +0530

    changes

--- Step-3 Solve problems on array/Lecture-2/Maximum_subarray_sum.cpp
+++ Step-3 Solve problems on array/Lecture-2/Maximum_subarray_sum.cpp
@@ -1,3 +1,87 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int maxSubArray(vector<int> &nums)
+  {
+
+    int maxi = INT_MIN;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      for (int j = i; j < nums.size(); j++)
+      {
+        int sum = 0;
+
+        for (int k = i; k <= j; k++)
+        {
+          sum += nums[k];
+        }
+
+        maxi = max(maxi, sum);
+      }
+    }
+
+    return maxi;
+  }
+};
+
+int main()
+{
+  vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+  Solution sol;
+
+  int maxSum = sol.maxSubArray(arr);
+
+  cout << "The maximum subarray sum is: " << maxSum << endl;
+  return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int maxSubArray(vector<int> &nums)
+  {
+
+    int maxi = INT_MIN;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+      int sum = 0;
+      for (int j = i; j < nums.size(); j++)
+      {
+        sum += nums[j];
+        maxi = max(maxi, sum);
+      }
+    }
+
+    return maxi;
+  }
+};
+
+int main()
+{
+  vector<int> arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+  Solution sol;
+
+  int maxSum = sol.maxSubArray(arr);
+
+  cout << "The maximum subarray sum is: " << maxSum << endl;
+
+  return 0;
+}
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,20 +91,11 @@ long long maxSubarraySum(int arr[], int n)
   for (int i = 0; i < n; i++)
   {
     sum += arr[i];
-
-    if (sum > maxi)
-    {
-      maxi = sum;
-    }
+    maxi = max(maxi, sum);
 
     if (sum < 0)
     {
       sum = 0;
-    }
-
-    if (maxi < 0)
-    {
-      maxi = 0;
     }
   }
   return maxi;
