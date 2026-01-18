59ad2238e2f71086f843dc4354779eb62be5eebe
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-01-18 23:30:13 +0530

    changes

--- Step-3 Solve problems on array/Lecture-2/Rearrange_the_array_in_alternate.cpp
+++ Step-3 Solve problems on array/Lecture-2/Rearrange_the_array_in_alternate.cpp
@@ -1,4 +1,50 @@
 // Equal no. of positive and negative integer
+#include <bits/stdc++.h>
+using namespace std;
+
+class ArrayManipulator
+{
+public:
+  vector<int> RearrangeBySign(vector<int> &A, int n)
+  {
+    vector<int> pos;
+    vector<int> neg;
+
+    for (int i = 0; i < n; i++)
+    {
+      if (A[i] > 0)
+        pos.push_back(A[i]);
+      else
+        neg.push_back(A[i]);
+    }
+
+    for (int i = 0; i < n / 2; i++)
+    {
+      A[2 * i] = pos[i];
+      A[2 * i + 1] = neg[i];
+    }
+
+    return A;
+  }
+};
+
+int main()
+{
+  int n = 4;
+  vector<int> A{1, 2, -4, -5};
+
+  ArrayManipulator obj;
+
+  vector<int> ans = obj.RearrangeBySign(A, n);
+
+  for (int i = 0; i < ans.size(); i++)
+  {
+    cout << ans[i] << " ";
+  }
+
+  return 0;
+}
+
 /*
 #include <bits/stdc++.h>
 using namespace std;
