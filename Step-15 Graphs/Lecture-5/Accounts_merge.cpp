1a41ddc70132f00bdb41539ead803d0f1cd73401
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-01-28 23:50:02 +0530

    Accounts merge

---
+++ Step-15 Graphs/Lecture-5/Accounts_merge.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet
+{
+  vector<int> size, parent;
+
+public:
+  DisjointSet(int n)
+  {
+    size.resize(n + 1);
+    parent.resize(n + 1);
+    for (int i = 0; i <= n; i++)
+    {
+      parent[i] = i;
+      size[i] = 1;
+    }
+  }
+
+  int findUPar(int node)
+  {
+    if (node == parent[node])
+      return node;
+    return parent[node] = findUPar(parent[node]);
+  }
+
+  void unionbySize(int u, int v)
+  {
+    int ulp_u = findUPar(u);
+    int ulp_v = findUPar(v);
+    if (ulp_u == ulp_v)
+      return;
+    if (size[ulp_u] < size[ulp_v])
+    {
+      parent[ulp_u] = ulp_v;
+      size[ulp_v] += size[ulp_u];
+    }
+    else
+    {
+      parent[ulp_v] = ulp_u;
+      size[ulp_u] += size[ulp_v];
+    }
+  }
+};
+
+class Solution
+{
+public:
+  vector<vector<string>> mergeDetails(vector<vector<string>> &details)
+  {
+    int n = details.size();
+    DisjointSet ds(n);
+    unordered_map<string, int> mapMailNode;
+    for (int i = 0; i < n; i++)
+    {
+      for (int j = 1; j < details[i].size(); j++)
+      {
+        string mail = details[i][j];
+        if (mapMailNode.find(mail) == mapMailNode.end())
+        {
+          mapMailNode[mail] = i;
+        }
+        else
+        {
+          ds.unionbySize(i, mapMailNode[mail]);
+        }
+      }
+    }
+    vector<string> mergedMail[n];
+    for (auto it : mapMailNode)
+    {
+      string mail = it.first;
+      int node = ds.findUPar(it.second);
+      mergedMail[node].push_back(mail);
+    }
+
+    vector<vector<string>> ans;
+
+    for (int i = 0; i < n; i++)
+    {
+      if (mergedMail[i].size() == 0)
+        continue;
+      sort(mergedMail[i].begin(), mergedMail[i].end());
+      vector<string> temp;
+      temp.push_back(details[i][0]);
+      for (auto it : mergedMail[i])
+      {
+        temp.push_back(it);
+      }
+      ans.push_back(temp);
+    }
+    return ans;
+  }
+};
+
+int main()
+{
+  vector<vector<string>> accounts = {
+      {"John", "j1@com", "j2@com", "j3@com"},
+      {"John", "j4@com"},
+      {"Raj", "r1@com", "r2@com"},
+      {"John", "j1@com", "j5@com"},
+      {"Raj", "r2@com", "r3@com"},
+      {"Mary", "m1@com"}};
+
+  Solution obj;
+
+  vector<vector<string>> ans = obj.mergeDetails(accounts);
+
+  for (auto &acc : ans)
+  {
+    cout << acc[0] << ":";
+    for (int i = 1; i < acc.size(); i++)
+    {
+      cout << acc[i] << " ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
