53e6ccce662627d5828ba87f8b0f3a20585816de
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-01-28 16:37:35 +0530

    graph

--- Step-15 Graphs/Lecture-5/Number_of_operations_to_make_network_connected.cpp
+++ Step-15 Graphs/Lecture-5/Number_of_operations_to_make_network_connected.cpp
@@ -1,7 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class DisjointSet
+{
+  vector<int> size, parent;
+
+public:
+  DisjointSet(int n)
+  {
+    size.resize(n + 1);
+    parent.resize(n + 1);
+    for (int i = 0; i <= n; i++)
+    {
+      parent[i] = i;
+      size[i] = 1;
+    }
+  }
+
+  int findUPar(int node)
+  {
+    if (node == parent[node])
+      return node;
+    return parent[node] = findUPar(parent[node]);
+  }
+
+  void unionbySize(int u, int v)
+  {
+    int ulp_u = findUPar(u);
+    int ulp_v = findUPar(v);
+    if (ulp_u == ulp_v)
+      return;
+    if (size[ulp_u] < size[ulp_v])
+    {
+      parent[ulp_u] = ulp_v;
+      size[ulp_v] += size[ulp_u];
+    }
+    else
+    {
+      parent[ulp_v] = ulp_u;
+      size[ulp_u] += size[ulp_v];
+    }
+  }
+};
+
 class Solution
 {
 public:
+  int Solve(int n, vector<vector<int>> &edge)
+  {
+    DisjointSet ds(n);
+    int cntExtras = 0;
+    for (auto it : edge)
+    {
+      int u = it[0];
+      int v = it[1];
+      if (ds.findUPar(u) == ds.findUPar(v))
+      {
+        cntExtras++;
+      }
+      else
+      {
+        ds.unionbySize(u, v);
+      }
+    }
+    int cntC = 0;
+    for (int i = 0; i < n; i++)
+    {
+      if (ds.findUPar(i) == i)
+        cntC++;
+    }
+    int ans = cntC - 1;
+    if (cntExtras >= ans)
+      return ans;
+    return -1;
+  }
 };
+
+int main()
+{
+  int n = 6;
+  vector<vector<int>> connections = {{0, 1}, {0, 2}, {0, 3}, {1, 4}};
+  Solution sol;
+  cout << sol.Solve(n, connections) << endl;
+  return 0;
+}
