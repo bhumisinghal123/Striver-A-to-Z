ef861a3ee757647492fe0a3cef2b6da77856900d
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-02-01 20:19:58 +0530

    changes

--- Step-3 Solve problems on array/Lecture-3/Merge_Overlapping_Subintervals.cpp
+++ Step-3 Solve problems on array/Lecture-3/Merge_Overlapping_Subintervals.cpp
@@ -2,47 +2,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr) {
-    int n = arr.size(); // size of the array
+class Solution
+{
+public:
+  vector<vector<int>> merge(vector<vector<int>> &intervals)
+  {
 
-    //sort the given intervals:
-    sort(arr.begin(), arr.end());
+    sort(intervals.begin(), intervals.end());
 
     vector<vector<int>> ans;
 
-    for (int i = 0; i < n; i++) { // select an interval:
-        int start = arr[i][0];
-        int end = arr[i][1];
+    int n = intervals.size();
+    for (int i = 0; i < n;)
+    {
 
-        //Skip all the merged intervals:
-        if (!ans.empty() && end <= ans.back()[1]) {
-            continue;
-        }
+      int start = intervals[i][0];
+      int end = intervals[i][1];
 
-        //check the rest of the intervals:
-        for (int j = i + 1; j < n; j++) {
-            if (arr[j][0] <= end) {
-                end = max(end, arr[j][1]);
-            }
-            else {
-                break;
-            }
-        }
-        ans.push_back({start, end});
+      int j = i + 1;
+      while (j < n && intervals[j][0] <= end)
+      {
+        end = max(end, intervals[j][1]);
+        j++;
+      }
+
+      ans.push_back({start, end});
+
+      i = j;
     }
+
     return ans;
-}
+  }
+};
 
 int main()
 {
-    vector<vector<int>> arr = {{1, 3}, {8, 10}, {2, 6}, {15, 18}};
-    vector<vector<int>> ans = mergeOverlappingIntervals(arr);
-    cout << "The merged intervals are: " << "\n";
-    for (auto it : ans) {
-        cout << "[" << it[0] << ", " << it[1] << "] ";
-    }
-    cout << endl;
-    return 0;
+  Solution sol;
+  vector<vector<int>> intervals = {{1, 3}, {2, 6}, {8, 10}, {15, 18}};
+  vector<vector<int>> result = sol.merge(intervals);
+
+  for (auto interval : result)
+  {
+    cout << "[" << interval[0] << "," << interval[1] << "] ";
+  }
+  return 0;
 }
 */
 
@@ -51,23 +54,18 @@ using namespace std;
 
 vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr)
 {
-  int n = arr.size(); // size of the array
+  int n = arr.size();
 
-  // sort the given intervals:
   sort(arr.begin(), arr.end());
 
   vector<vector<int>> ans;
 
   for (int i = 0; i < n; i++)
   {
-    // if the current interval does not
-    // lie in the last interval:
     if (ans.empty() || arr[i][0] > ans.back()[1])
     {
       ans.push_back(arr[i]);
     }
-    // if the current interval
-    // lies in the last interval:
     else
     {
       ans.back()[1] = max(ans.back()[1], arr[i][1]);
--- Step-3 Solve problems on array/Lecture-3/Merge_two_sorted_arrays_without_extra_space.cpp
+++ Step-3 Solve problems on array/Lecture-3/Merge_two_sorted_arrays_without_extra_space.cpp
@@ -4,16 +4,12 @@ using namespace std;
 
 void merge(long long arr1[], long long arr2[], int n, int m) {
 
-    //Declare a 3rd array and 2 pointers:
     long long arr3[n + m];
     int left = 0;
     int right = 0;
 
     int index = 0;
 
-    //Insert the elements from the 2 arrays
-    // into the 3rd array using left and right
-    // pointers:
 
     while (left < n && right < m) {
         if (arr1[left] <= arr2[right]) {
@@ -26,18 +22,14 @@ void merge(long long arr1[], long long arr2[], int n, int m) {
         }
     }
 
-    // If right pointer reaches the end:
     while (left < n) {
         arr3[index++] = arr1[left++];
     }
 
-    // If left pointer reaches the end:
     while (right < m) {
         arr3[index++] = arr2[right++];
     }
 
-    // Fill back the elements from arr3[]
-    // to arr1[] and arr2[]:
     for (int i = 0; i < n + m; i++) {
         if (i < n) arr1[i] = arr3[i];
         else arr2[i - n] = arr3[i];
@@ -71,12 +63,9 @@ using namespace std;
 void merge(long long arr1[], long long arr2[], int n, int m)
 {
 
-  // Declare 2 pointers:
   int left = n - 1;
   int right = 0;
 
-  // Swap the elements until arr1[left] is
-  //  smaller than arr2[right]:
   while (left >= 0 && right < m)
   {
     if (arr1[left] > arr2[right])
@@ -90,7 +79,6 @@ void merge(long long arr1[], long long arr2[], int n, int m)
     }
   }
 
-  // Sort arr1[] and arr2[] individually:
   sort(arr1, arr1 + n);
   sort(arr2, arr2 + m);
 }
@@ -117,78 +105,48 @@ int main()
 }
 */
 
-/*
 #include <bits/stdc++.h>
 using namespace std;
 
-void swapIfGreater(long long arr1[], long long arr2[], int ind1, int ind2)
+class Solution
 {
-  if (arr1[ind1] > arr2[ind2])
+public:
+  void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
   {
-    swap(arr1[ind1], arr2[ind2]);
-  }
-}
+    int i = m - 1;
 
-void merge(long long arr1[], long long arr2[], int n, int m)
-{
-  // len of the imaginary single array:
-  int len = n + m;
+    int j = n - 1;
 
-  // Initial gap:
-  int gap = (len / 2) + (len % 2);
+    int k = m + n - 1;
 
-  while (gap > 0)
-  {
-    // Place 2 pointers:
-    int left = 0;
-    int right = left + gap;
-    while (right < len)
+    while (i >= 0 && j >= 0)
     {
-      // case 1: left in arr1[]
-      // and right in arr2[]:
-      if (left < n && right >= n)
+      if (nums1[i] > nums2[j])
       {
-        swapIfGreater(arr1, arr2, left, right - n);
+        nums1[k--] = nums1[i--];
       }
-      // case 2: both pointers in arr2[]:
-      else if (left >= n)
-      {
-        swapIfGreater(arr2, arr2, left - n, right - n);
-      }
-      // case 3: both pointers in arr1[]:
       else
       {
-        swapIfGreater(arr1, arr1, left, right);
+        nums1[k--] = nums2[j--];
       }
-      left++, right++;
     }
-    // break if iteration gap=1 is completed:
-    if (gap == 1)
-      break;
 
-    // Otherwise, calculate new gap:
-    gap = (gap / 2) + (gap % 2);
+    while (j >= 0)
+    {
+      nums1[k--] = nums2[j--];
+    }
   }
-}
+};
 
 int main()
 {
-  long long arr1[] = {1, 4, 8, 10};
-  long long arr2[] = {2, 3, 9};
-  int n = 4, m = 3;
-  merge(arr1, arr2, n, m);
-  cout << "The merged arrays are: " << "\n";
-  cout << "arr1[] = ";
-  for (int i = 0; i < n; i++)
-  {
-    cout << arr1[i] << " ";
-  }
-  cout << "\narr2[] = ";
-  for (int i = 0; i < m; i++)
-  {
-    cout << arr2[i] << " ";
-  }
-  cout << endl;
+  vector<int> nums1 = {1, 3, 5, 0, 0, 0};
+  vector<int> nums2 = {2, 4, 6};
+  int m = 3, n = 3;
+
+  Solution().merge(nums1, m, nums2, n);
+
+  for (int num : nums1)
+    cout << num << " ";
   return 0;
 }
-*/
