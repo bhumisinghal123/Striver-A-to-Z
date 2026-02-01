1f3474fd362fe8ecc77b37c45501b67096514d93
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-02-01 20:20:14 +0530

    Maximum sum of non-adjacent elements (DP 5)

--- Step-16 Dynamic Programming/Lecture-2/Maximum_sum_of_non_adjacent_elements.cpp
+++ Step-16 Dynamic Programming/Lecture-2/Maximum_sum_of_non_adjacent_elements.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -27,4 +28,78 @@ int main()
 {
   vector<int> nums = {1, 3, 2, 8, 9};
   cout << maximumNonAdjacentSum(nums);
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int maximumNonAdjacentSum(vector<int> &arr)
+  {
+    int n = arr.size();
+
+    if (n == 1)
+      return arr[0];
+
+    vector<int> dp(n);
+
+    dp[0] = arr[0];
+
+    dp[1] = max(arr[0], arr[1]);
+
+    for (int i = 2; i < n; i++)
+    {
+      dp[i] = max(arr[i] + dp[i - 2], dp[i - 1]);
+    }
+
+    return dp[n - 1];
+  }
+};
+
+int main()
+{
+  vector<int> arr = {2, 1, 4, 9};
+  Solution obj;
+  cout << obj.maximumNonAdjacentSum(arr);
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int maximumNonAdjacentSum(vector<int> &nums)
+  {
+    int n = nums.size();
+    int prev = nums[0];
+    int prev2 = 0;
+    for (int i = 1; i < n; i++)
+    {
+      int take = nums[i];
+      if (i > 1)
+        take += prev2;
+
+      int notTake = 0 + prev;
+
+      int curi = max(take, notTake);
+      prev2 = prev;
+      prev = curi;
+    }
+    return prev;
+  }
+};
+
+int main()
+{
+  vector<int> arr = {2, 1, 4, 9};
+  Solution obj;
+  cout << obj.maximumNonAdjacentSum(arr);
+  return 0;
 }
