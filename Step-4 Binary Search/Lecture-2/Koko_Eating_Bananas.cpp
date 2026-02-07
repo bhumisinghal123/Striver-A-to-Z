42f0b236653a1932d9748dd5ffbd732f1c0cd70e
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-02-08 00:40:09 +0530

    changes

--- Step-4 Binary Search/Lecture-2/Koko_Eating_Bananas.cpp
+++ Step-4 Binary Search/Lecture-2/Koko_Eating_Bananas.cpp
@@ -6,7 +6,6 @@ int findMax(vector<int> &v)
 {
   int maxi = INT_MIN;
   int n = v.size();
-  // find the maximum:
   for (int i = 0; i < n; i++)
   {
     maxi = max(maxi, v[i]);
@@ -18,7 +17,6 @@ int calculateTotalHours(vector<int> &v, int hourly)
 {
   int totalH = 0;
   int n = v.size();
-  // find total hours:
   for (int i = 0; i < n; i++)
   {
     totalH += ceil((double)(v[i]) / (double)(hourly));
@@ -28,10 +26,8 @@ int calculateTotalHours(vector<int> &v, int hourly)
 
 int minimumRateToEatBananas(vector<int> v, int h)
 {
-  // Find the maximum number:
   int maxi = findMax(v);
 
-  // Find the minimum value of k:
   for (int i = 1; i <= maxi; i++)
   {
     int reqTime = calculateTotalHours(v, i);
@@ -41,7 +37,6 @@ int minimumRateToEatBananas(vector<int> v, int h)
     }
   }
 
-  // dummy return statement
   return maxi;
 }
 
--- Step-4 Binary Search/Lecture-2/Minimum_days_to_make_M_bouquets.cpp
+++ Step-4 Binary Search/Lecture-2/Minimum_days_to_make_M_bouquets.cpp
@@ -4,10 +4,9 @@ using namespace std;
 
 bool possible(vector<int> &arr, int day, int m, int k)
 {
-  int n = arr.size(); // size of the array
+  int n = arr.size();
   int cnt = 0;
   int noOfB = 0;
-  // count the number of bouquets:
   for (int i = 0; i < n; i++)
   {
     if (arr[i] <= day)
@@ -26,10 +25,9 @@ bool possible(vector<int> &arr, int day, int m, int k)
 int roseGarden(vector<int> arr, int k, int m)
 {
   long long val = m * 1ll * k * 1ll;
-  int n = arr.size(); // size of the array
+  int n = arr.size();
   if (val > n)
-    return -1; // impossible case.
-  // find maximum and minimum:
+    return -1;
   int mini = INT_MAX, maxi = INT_MIN;
   for (int i = 0; i < n; i++)
   {
