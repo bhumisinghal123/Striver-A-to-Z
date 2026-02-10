b92142dfc4345d8a37745ff815b01aa126bf019b
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-02-10 23:15:30 +0530

    changes

--- Step-4 Binary Search/Lecture-2/Painter_partition.cpp
+++ Step-4 Binary Search/Lecture-2/Painter_partition.cpp
@@ -1,3 +1,60 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class PainterPartition
+{
+public:
+  int countPainters(vector<int> &boards, int time)
+  {
+    int n = boards.size();
+    int painters = 1;
+    long long boardsPainter = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+      if (boardsPainter + boards[i] <= time)
+      {
+        boardsPainter += boards[i];
+      }
+      else
+      {
+        painters++;
+        boardsPainter = boards[i];
+      }
+    }
+    return painters;
+  }
+
+  int findLargestMinDistance(vector<int> &boards, int k)
+  {
+    int low = *max_element(boards.begin(), boards.end());
+    int high = accumulate(boards.begin(), boards.end(), 0);
+
+    for (int time = low; time <= high; time++)
+    {
+      if (countPainters(boards, time) <= k)
+      {
+        return time;
+      }
+    }
+    return low;
+  }
+};
+
+int main()
+{
+  vector<int> boards = {10, 20, 30, 40};
+  int k = 2;
+
+  PainterPartition obj;
+  int ans = obj.findLargestMinDistance(boards, k);
+  cout << "The answer is: " << ans << "\n";
+
+  return 0;
+}
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
--- Step-4 Binary Search/Lecture-2/Split_array_Largest_Sum.cpp
+++ Step-4 Binary Search/Lecture-2/Split_array_Largest_Sum.cpp
@@ -1,3 +1,56 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class SubarrayPartitioner
+{
+public:
+  int countPartitions(vector<int> &a, int maxSum)
+  {
+    int n = a.size();
+    int partitions = 1;
+    long long subarraySum = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+      if (subarraySum + a[i] <= maxSum)
+      {
+        subarraySum += a[i];
+      }
+      else
+      {
+        partitions++;
+        subarraySum = a[i];
+      }
+    }
+    return partitions;
+  }
+
+  int largestSubarraySumMinimized(vector<int> &a, int k)
+  {
+    int low = *max_element(a.begin(), a.end());
+    int high = accumulate(a.begin(), a.end(), 0);
+
+    for (int maxSum = low; maxSum <= high; maxSum++)
+    {
+      if (countPartitions(a, maxSum) == k)
+        return maxSum;
+    }
+    return low;
+  }
+};
+
+int main()
+{
+  vector<int> a = {10, 20, 30, 40};
+  int k = 2;
+  SubarrayPartitioner sp;
+  int ans = sp.largestSubarraySumMinimized(a, k);
+  cout << "The answer is: " << ans << "\n";
+  return 0;
+}
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
