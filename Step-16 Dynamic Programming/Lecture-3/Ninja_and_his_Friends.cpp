446f81060708231fcc01bed10798ddc57499fc61
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-02-10 22:18:02 +0530

    Ninja and his Friends

---
+++ Step-16 Dynamic Programming/Lecture-3/Ninja_and_his_Friends.cpp
@@ -0,0 +1,190 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int f(int i, int j1, int j2, int r, int c, vector<vector<int>> &grid)
+  {
+    if (j1 < 0 || j2 < 0 || j1 >= c || j2 >= c)
+    {
+      return -1e8;
+    }
+
+    if (i == r - 1)
+    {
+      if (j1 == j2)
+        return grid[i][j1];
+      else
+        return grid[i][j1] + grid[i][j2];
+    }
+
+    int maxi = -1e8;
+    for (int dj1 = -1; dj1 <= +1; dj1++)
+    {
+      for (int dj2 = -1; dj2 <= +1; dj2++)
+      {
+        int value = 0;
+        if (j1 == j2)
+          value = grid[i][j1];
+        else
+          value = grid[i][j1] + grid[i][j2];
+        value += f(i + 1, j1 + dj1, j2 + dj2, r, c, grid);
+        maxi = max(maxi, value);
+      }
+    }
+    return maxi;
+  }
+
+  int maximumChocolates(int r, int c, vector<vector<int>> &grid)
+  {
+    return f(0, 0, c - 1, r, c, grid);
+  }
+};
+
+int main()
+{
+  vector<vector<int>> grid = {
+      {2, 3, 1, 2},
+      {3, 4, 2, 2},
+      {5, 6, 3, 5}};
+  int n = grid.size(), m = grid[0].size();
+  Solution obj;
+  cout << obj.maximumChocolates(n, m, grid) << endl;
+  return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int f(int i, int j1, int j2, int r, int c, vector<vector<int>> &grid, vector<vector<vector<int>>> &dp)
+  {
+    if (j1 < 0 || j2 < 0 || j1 >= c || j2 >= c)
+    {
+      return -1e8;
+    }
+
+    if (i == r - 1)
+    {
+      if (j1 == j2)
+        return grid[i][j1];
+      else
+        return grid[i][j1] + grid[i][j2];
+    }
+
+    if (dp[i][j1][j2] != -1)
+      return dp[i][j1][j2];
+
+    int maxi = -1e8;
+    for (int dj1 = -1; dj1 <= +1; dj1++)
+    {
+      for (int dj2 = -1; dj2 <= +1; dj2++)
+      {
+        int value = 0;
+        if (j1 == j2)
+          value = grid[i][j1];
+        else
+          value = grid[i][j1] + grid[i][j2];
+        value += f(i + 1, j1 + dj1, j2 + dj2, r, c, grid, dp);
+        maxi = max(maxi, value);
+      }
+    }
+    return dp[i][j1][j2] = maxi;
+  }
+
+  int maximumChocolates(int r, int c, vector<vector<int>> &grid)
+  {
+    vector<vector<vector<int>>> dp(r, vector<vector<int>>(c, vector<int>(c, -1)));
+    return f(0, 0, c - 1, r, c, grid, dp);
+  }
+};
+
+int main()
+{
+  vector<vector<int>> grid = {
+      {2, 3, 1, 2},
+      {3, 4, 2, 2},
+      {5, 6, 3, 5}};
+  int n = grid.size(), m = grid[0].size();
+  Solution obj;
+  cout << obj.maximumChocolates(n, m, grid) << endl;
+  return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+  int maximumChocolates(int n, int m, vector<vector<int>> &grid)
+  {
+    vector<vector<vector<int>>> dp(
+        n, vector<vector<int>>(m, vector<int>(m, -1)));
+
+    for (int j1 = 0; j1 < m; j1++)
+    {
+      for (int j2 = 0; j2 < m; j2++)
+      {
+        if (j1 == j2)
+          dp[n - 1][j1][j2] = grid[n - 1][j1];
+        else
+          dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+      }
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+      for (int j1 = 0; j1 < m; j1++)
+      {
+        for (int j2 = 0; j2 < m; j2++)
+        {
+          int maxi = -1e8;
+
+          for (int dj1 = -1; dj1 <= 1; dj1++)
+          {
+            for (int dj2 = -1; dj2 <= 1; dj2++)
+            {
+              int nj1 = j1 + dj1;
+              int nj2 = j2 + dj2;
+
+              if (nj1 >= 0 && nj1 < m && nj2 >= 0 && nj2 < m)
+              {
+                int value = (j1 == j2)
+                                ? grid[i][j1]
+                                : grid[i][j1] + grid[i][j2];
+
+                value += dp[i + 1][nj1][nj2];
+                maxi = max(maxi, value);
+              }
+            }
+          }
+
+          dp[i][j1][j2] = maxi;
+        }
+      }
+    }
+
+    return dp[0][0][m - 1];
+  }
+};
+
+int main()
+{
+  vector<vector<int>> grid = {
+      {2, 3, 1, 2},
+      {3, 4, 2, 2},
+      {5, 6, 3, 5}};
+
+  int n = grid.size(), m = grid[0].size();
+  Solution obj;
+  cout << obj.maximumChocolates(n, m, grid) << endl;
+  return 0;
+}
