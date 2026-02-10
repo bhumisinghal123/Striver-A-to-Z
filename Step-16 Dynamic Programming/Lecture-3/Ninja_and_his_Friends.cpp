8baed50c4f78b8b56f8c207b61634e8322e41f04
Bhumi Singhal <bhumisinghal02@gmail.com>
2026-02-10 22:20:32 +0530

    changes

--- Step-16 Dynamic Programming/Lecture-3/Ninja_and_his_Friends.cpp
+++ Step-16 Dynamic Programming/Lecture-3/Ninja_and_his_Friends.cpp
@@ -126,17 +126,17 @@ class Solution
 public:
   int maximumChocolates(int n, int m, vector<vector<int>> &grid)
   {
-    vector<vector<vector<int>>> dp(
-        n, vector<vector<int>>(m, vector<int>(m, -1)));
+    vector<vector<int>> next(m, vector<int>(m, 0));
+    vector<vector<int>> curr(m, vector<int>(m, 0));
 
     for (int j1 = 0; j1 < m; j1++)
     {
       for (int j2 = 0; j2 < m; j2++)
       {
         if (j1 == j2)
-          dp[n - 1][j1][j2] = grid[n - 1][j1];
+          next[j1][j2] = grid[n - 1][j1];
         else
-          dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+          next[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
       }
     }
 
@@ -146,33 +146,33 @@ public:
       {
         for (int j2 = 0; j2 < m; j2++)
         {
-          int maxi = -1e8;
-
+          int maxi = -1e9;
+          int currChoco = (j1 == j2) ? grid[i][j1]
+                                     : grid[i][j1] + grid[i][j2];
           for (int dj1 = -1; dj1 <= 1; dj1++)
           {
             for (int dj2 = -1; dj2 <= 1; dj2++)
             {
-              int nj1 = j1 + dj1;
-              int nj2 = j2 + dj2;
-
-              if (nj1 >= 0 && nj1 < m && nj2 >= 0 && nj2 < m)
+              int newJ1 = j1 + dj1;
+              int newJ2 = j2 + dj2;
+              if (newJ1 >= 0 && newJ1 < m &&
+                  newJ2 >= 0 && newJ2 < m)
               {
-                int value = (j1 == j2)
-                                ? grid[i][j1]
-                                : grid[i][j1] + grid[i][j2];
-
-                value += dp[i + 1][nj1][nj2];
-                maxi = max(maxi, value);
+                maxi = max(maxi, currChoco +
+                                     next[newJ1][newJ2]);
+              }
+              else
+              {
+                maxi = max(maxi, (int)-1e9);
               }
             }
           }
-
-          dp[i][j1][j2] = maxi;
+          curr[j1][j2] = maxi;
         }
       }
+      next = curr;
     }
-
-    return dp[0][0][m - 1];
+    return next[0][m - 1];
   }
 };
 
@@ -182,7 +182,6 @@ int main()
       {2, 3, 1, 2},
       {3, 4, 2, 2},
       {5, 6, 3, 5}};
-
   int n = grid.size(), m = grid[0].size();
   Solution obj;
   cout << obj.maximumChocolates(n, m, grid) << endl;
