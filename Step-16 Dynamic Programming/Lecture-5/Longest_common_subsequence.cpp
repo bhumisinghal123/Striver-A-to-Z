05997420ba9e785eab36a79794c26d6e3b2b2901
bhumisinghal123 <bhumisinghal02@gmail.com>
2026-04-08 22:57:25 +0530

    Longest common subsequence

--- Step-16 Dynamic Programming/Lecture-5/Longest_common_subsequence.cpp
+++ Step-16 Dynamic Programming/Lecture-5/Longest_common_subsequence.cpp
@@ -4,51 +4,26 @@ using namespace std;
 class Solution
 {
 public:
-    string longestCommonSubsequence(string &text1, string &text2)
+    int longestCommonSubsequence(string &s, string &t)
     {
-        int n = text1.size();
-        int m = text2.size();
-
+        int n = s.size();
+        int m = t.size();
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
+        for (int j = 0; j <= m; j++)
+            dp[0][j] = 0;
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = 0;
         for (int i = 1; i <= n; i++)
         {
             for (int j = 1; j <= m; j++)
             {
-                if (text1[i - 1] == text2[j - 1])
-                {
+                if (s[i - 1] == t[j - 1])
                     dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
                 else
-                {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
             }
         }
-
-        int i = n, j = m;
-        string lcs = "";
-
-        while (i > 0 && j > 0)
-        {
-            if (text1[i - 1] == text2[j - 1])
-            {
-                lcs += text1[i - 1];
-                i--;
-                j--;
-            }
-            else if (dp[i - 1][j] > dp[i][j - 1])
-            {
-                i--;
-            }
-            else
-            {
-                j--;
-            }
-        }
-        reverse(lcs.begin(), lcs.end());
-
-        return lcs;
+        return dp[n][m];
     }
 };
 
