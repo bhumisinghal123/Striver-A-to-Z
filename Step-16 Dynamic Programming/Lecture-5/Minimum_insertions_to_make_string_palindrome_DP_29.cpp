bda42f8777b5a5b9133944e454703e91488474fe
bhumisinghal123 <bhumisinghal02@gmail.com>
2026-04-08 19:56:52 +0530

    Minimum insertions to make string palindrome | DP-29

---
+++ Step-16 Dynamic Programming/Lecture-5/Minimum_insertions_to_make_string_palindrome_DP_29.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int lcs(string s, string t)
+{
+    int n = s.size();
+    int m = t.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    for (int j = 0; j <= m; j++)
+        dp[0][j] = 0;
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s[i - 1] == t[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+        }
+    }
+    return dp[n][m];
+}
+
+int longestPalindromeSubsequence(string s)
+{
+    string t = s;
+    reverse(t.begin(), t.end());
+    return lcs(s, t);
+}
+
+int minInsertion(string &str)
+{
+    return str.size() - longestPalindromeSubsequence(str);
+}
+
+int main()
+{
+    string s = "abcaa";
+    cout << "Minimum operations: " << minInsertion(s) << endl;
+    return 0;
+}
