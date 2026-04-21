482049df708e13024726d98e14d73106453b8c21
bhumisinghal123 <bhumisinghal02@gmail.com>
2026-04-21 18:08:33 +0530

    LCS

--- Step-16 Dynamic Programming/Lecture-5/Longest_common_subsequence.cpp
+++ Step-16 Dynamic Programming/Lecture-5/Longest_common_subsequence.cpp
@@ -1,3 +1,86 @@
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int f(int i, int j, string &s, string &t)
+    {
+        if (i < 0 || j < 0)
+            return 0;
+
+        if (s[i] == t[j])
+            return 1 + f(i - 1, j - 1, s, t);
+
+        return max(f(i - 1, j, s, t), f(i, j - 1, s, t));
+    }
+
+    int longestCommonSubsequence(string &s, string &t)
+    {
+        int n = s.size();
+        int m = t.size();
+        return f(n - 1, m - 1, s, t);
+    }
+};
+
+int main()
+{
+    string s1 = "abcde";
+    string s2 = "ace";
+
+    Solution sol;
+    cout << "LCS:" << sol.longestCommonSubsequence(s1, s2) << endl;
+
+    return 0;
+}
+*/
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int f(int i, int j, string &s, string &t, vector<vector<int>> &dp)
+    {
+        if (i == 0 || j == 0)
+            return 0;
+
+        if (dp[i][j] != -1)
+        {
+            return dp[i][j];
+        }
+
+        if (s[i-1] == t[j-1])
+            return dp[i][j] = 1 + f(i - 1, j - 1, s, t, dp);
+
+        return dp[i][j] = max(f(i - 1, j, s, t, dp), f(i, j - 1, s, t, dp));
+    }
+
+    int longestCommonSubsequence(string &s, string &t)
+    {
+        int n = s.size();
+        int m = t.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return f(n, m, s, t, dp);
+    }
+};
+
+int main()
+{
+    string s1 = "abcde";
+    string s2 = "ace";
+
+    Solution sol;
+    cout << "LCS:" << sol.longestCommonSubsequence(s1, s2) << endl;
+
+    return 0;
+}
+*/
+
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,6 +92,7 @@ public:
         int n = s.size();
         int m = t.size();
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
         for (int j = 0; j <= m; j++)
             dp[0][j] = 0;
         for (int i = 0; i <= n; i++)
@@ -20,10 +104,54 @@ public:
                 if (s[i - 1] == t[j - 1])
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 else
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j]);
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
+
         return dp[n][m];
+    }
+};
+
+int main()
+{
+    string s1 = "abcde";
+    string s2 = "ace";
+
+    Solution sol;
+    cout << "LCS:" << sol.longestCommonSubsequence(s1, s2) << endl;
+
+    return 0;
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int longestCommonSubsequence(string &s, string &t)
+    {
+        int n = s.size();
+        int m = t.size();
+        vector<int> prev(m + 1, 0), cur(m + 1, 0);
+
+        for (int j = 0; j <= m; j++)
+            prev[j] = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= m; j++)
+            {
+                if (s[i - 1] == t[j - 1])
+                    cur[j] = 1 + prev[j - 1];
+                else
+                    cur[j] = max(prev[j], cur[j - 1]);
+            }
+            prev = cur;
+        }
+
+        return prev[m];
     }
 };
 
