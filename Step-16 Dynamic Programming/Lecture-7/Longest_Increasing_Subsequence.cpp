8f43b93fd0489b8c2a8059566959fbb845c55ada
bhumisinghal123 <bhumisinghal02@gmail.com>
2026-04-23 19:27:39 +0530

    Longest Increasing Subsequence

--- Step-16 Dynamic Programming/Lecture-7/Longest_Increasing_Subsequence.cpp
+++ Step-16 Dynamic Programming/Lecture-7/Longest_Increasing_Subsequence.cpp
@@ -1,7 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int f(int ind, int prev_ind, vector<int> &arr, int n)
+{
+    if (ind == n)
+        return 0;
+
+    int len = 0 + f(ind + 1, prev_ind, arr, n);
+    if (prev_ind == -1 || arr[ind] > arr[prev_ind])
+    {
+        len = max(len, 1 + f(ind + 1, ind, arr, n));
+    }
+    return len;
+}
+
+int longestIncreasingSubsequence(vector<int> &arr, int n)
+{
+    return f(0, -1, arr, n);
+}
+
 int main()
 {
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    int n = 8;
+    int lengthOfLIS = longestIncreasingSubsequence(nums, n);
+
+    cout << "The length of the LIS for the given array is: " << lengthOfLIS << endl;
+
     return 0;
 }
